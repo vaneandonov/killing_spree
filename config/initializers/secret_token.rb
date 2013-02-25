43e0079d2c1c24da6f8816ea69b2dabb003332a7
Vane Andonov <vane.andonov@gmail.com>
2013-02-25 08:27:56 +0100

    installed fresh spree store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KillingSpree::Application.config.secret_token = '708717018efbc25b02086183ed840023429eada9568e279bf347b88e0f4eb8ac9a0abd87749b0dc2e84847f83cfcba4eb495bc5a0f7f96a913708ffbab220ee9'
